a06fc30053175e2bb050e6e7a8b31f0ddd3f11fe
Matas Pigaga <tech@matas.io>
2021-11-05 13:44:02 -0400

    added static props

--- 01-react-basics/app.js
+++ 01-react-basics/app.js
@@ -1,26 +1,26 @@
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
+      <h1>{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
     </header>
   );
 };
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
-      <span className="player-name">Matas</span>
-      <Counter />
+      <span className="player-name">{props.name}</span>
+      <Counter score={props.score} />
     </div>
   );
 };
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{props.score}</span>
       <button className="counter-action increment"> - </button>
     </div>
   );
@@ -29,10 +29,13 @@ const Counter = () => {
 const App = () => {
   return (
     <div className="scoreboard">
-      <Header />
+      <Header title="Scoreboard" totalPlayers={1} />
 
       {/* Player's list */}
-      <Player />
+      <Player name="Matas" score={50} />
+      <Player name="Jenny" score={45} />
+      <Player name="Richard" score={20} />
+      <Player name="Dave" score={70} />
     </div>
   );
 };
