6f65efde5b1c5750ace9c505f05cdec4d9d1762e
Matas Pigaga <tech@matas.io>
2021-11-05 14:18:57 -0400

    updated to React 17 for hooks, added counter

--- 01-react-basics/app.js
+++ 01-react-basics/app.js
@@ -2,24 +2,26 @@ const players = [
   {
     name: "Guil",
     score: 50,
-    id: 1
+    id: 1,
   },
   {
     name: "Treasure",
     score: 85,
-    id: 2
+    id: 2,
   },
   {
     name: "Ashley",
     score: 95,
-    id: 3
+    id: 3,
   },
   {
     name: "James",
     score: 80,
-    id: 4
+    id: 4,
   },
 ];
+
+const { useState } = React;
 
 const Header = (props) => {
   return (
@@ -34,17 +36,31 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">{props.name}</span>
-      <Counter score={props.score} />
+      <Counter />
     </div>
   );
 };
 
 const Counter = (props) => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{props.score}</span>
-      <button className="counter-action increment"> - </button>
+      <button
+        className="counter-action decrement"
+        onClick={() => setCount((prevCount) => prevCount - 1)}
+      >
+        {" "}
+        -{" "}
+      </button>
+      <span className="counter-score">{count}</span>
+      <button
+        className="counter-action increment"
+        onClick={() => setCount((prevCount) => prevCount + 1)}
+      >
+        {" "}
+        -{" "}
+      </button>
     </div>
   );
 };
@@ -56,7 +72,7 @@ const App = (props) => {
 
       {/* Player's list */}
       {props.initialPlayers.map((player) => (
-        <Player name={player.name} score={player.score} key={player.id.toString()} />
+        <Player name={player.name} key={player.id.toString()} />
       ))}
     </div>
   );
